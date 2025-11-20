3cfd6d729cd10d4cc6b708fc3e7625948b61e363
Ayush-398 <175736269+Ayush-398@users.noreply.github.com>
2025-11-20 18:05:06 +0530

    Setup: trpc server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
-"use client";
 
 import Image from "next/image";
 import { prisma } from "@/lib/db";
 import { Prisma } from "@/generated/prisma/browser";
 import { useTRPC } from "@/trpc/client";
-const Page =  () =>{
+import { json } from "zod";
+import {trpc, getQueryClient} from "@/trpc/server"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+const Page = async () =>{
 
- const trpc = useTRPC();
- trpc.createAI.queryOptions({text:"Hello"});
+const queryClient = getQueryClient();
+void queryClient.prefetchQuery(trpc.createAI.queryOptions({text:"Hello world"}));
   return(
+    <HydrationBoundary state = {dehydrate(queryClient())}>
     <div>
-      Hello world
+     
     </div>
+    </HydrationBoundary>
   );
 }
 
